941313911399bd30848918b5521d70a1640a5746
harishgobugari <gharishreddy224@gmail.com>
2024-07-14 13:28:25 +0100

    folder structure commit

--- etl/main.py
+++ etl/main.py
@@ -4,10 +4,10 @@ def main():
     """This is the main function of the script."""
     
     vehicles_url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/TEM27/JSON-stat/2.0/en"
-    load_vehicle_data_to_csv(data_url=vehicles_url,file_path='vehicles_new_updated.csv')
+    load_vehicle_data_to_csv(data_url=vehicles_url,file_path='output/vehicles.csv')
 
     charging_points_pdf_url = 'https://esb.ie/what-we-do/ecars/charge-point-map'
-    load_charging_points_data_to_csv(data_url=charging_points_pdf_url,county_url=vehicles_url,file_path='charging_new_updated.csv')
+    load_charging_points_data_to_csv(data_url=charging_points_pdf_url,county_url=vehicles_url,file_path='output/charging_loactions.csv')
   
 
 if __name__ == '__main__':
